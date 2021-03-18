7f416fecb212a3a11b24dded41b512e180c253d9
DougWelchons <doug.welchons@gmail.com>
2021-03-18 17:15:23 -0600

    Add tests for logging in

--- spec/features/user/login_spec.rb
+++ spec/features/user/login_spec.rb
@@ -21,4 +21,26 @@ RSpec.describe "User Login:" do
       expect(current_path).to eq(login_path)
     end
   end
+
+  describe "when i visit the login page it" do
+    it "has a login form" do
+      visit login_path
+
+      within(".form") do
+        expect(page).to have_field(:email)
+        expect(page).to have_field(:password)
+        expect(page).to have_button("Login")
+      end
+    end
+
+    it "logs me in with valid info" do
+      visit login_path
+
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+      click_button("Login")
+
+      expect(current_path).to eq(user_path(@user))
+    end
+  end
 end
