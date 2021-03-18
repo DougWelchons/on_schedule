639b10ba56bfc38f28737a5fd137ac61a8ff567a
DougWelchons <doug.welchons@gmail.com>
2021-03-18 15:52:32 -0600

    Add model tests for user validations and relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should belong_to :company }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password }
+  end
 end
