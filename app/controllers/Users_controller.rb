1aadebfd56b5b09d9a1e8007f820cead124738d6
DougWelchons <doug.welchons@gmail.com>
2021-03-18 16:09:52 -0600

    add refactor to user controller for find_company

--- app/controllers/Users_controller.rb
+++ app/controllers/Users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  before_action :find_company, only: [:new, :create]
 
-  def new
+  def create
+    redirect_to company_path(@company)
+  end
+
+  private
+  def find_company
     @company = Company.find(params[:company_id])
-    @user = User.new(company: @company)
   end
 end
